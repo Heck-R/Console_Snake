abc28c44b594cc3a7caffeef9f3adca9588bc979
Heck-R <hecvid97@gmail.com>
2019-08-07 22:11:24 +0200

    Updated: Reworked game over looks a bit Updated: Game over's code is a bit more separated thus more viewable

--- Snake/Snake.pas
+++ Snake/Snake.pas
@@ -7,7 +7,7 @@ const max=10000;
 var harveg, opt:boolean;
 var c, b, omozg, nul, pmozg, pel:char;
 var otart, oel, gyor:integer;
-var ohosz:integer;
+var ohosz, lepes:integer;
 var mag, szel:integer;
 var x, y:integer;
 var m, m2:integer;
@@ -25,345 +25,345 @@ begin		//GAME OVER-------------------------------------------------
 delay(100);
 for i:=17 to 26 do
 	begin
-	gotoxy(i, 2);
+	gotoxy(i, 7);
 	write('*');
 	end;
 for i:=34 to 34 do
 	begin
-	gotoxy(i, 2);
+	gotoxy(i, 7);
 	write('*');
 	end;
 for i:=42 to 45 do
 	begin
-	gotoxy(i, 2);
+	gotoxy(i, 7);
 	write('*');
 	end;
 for i:=49 to 52 do
 	begin
-	gotoxy(i, 2);
+	gotoxy(i, 7);
 	write('*');
 	end;
 for i:=55 to 64 do
 	begin
-	gotoxy(i, 2);
+	gotoxy(i, 7);
 	write('*');
 	end;
 //2. sor
 delay(100);
 for i:=17 to 26 do
 	begin
-	gotoxy(i, 3);
+	gotoxy(i, 8);
 	write('*');
 	end;
 for i:=33 to 35 do
 	begin
-	gotoxy(i, 3);
+	gotoxy(i, 8);
 	write('*');
 	end;
 for i:=42 to 46 do
 	begin
-	gotoxy(i, 3);
+	gotoxy(i, 8);
 	write('*');
 	end;
 for i:=48 to 52 do
 	begin
-	gotoxy(i, 3);
+	gotoxy(i, 8);
 	write('*');
 	end;
 for i:=55 to 64 do
 	begin
-	gotoxy(i, 3);
+	gotoxy(i, 8);
 	write('*');
 	end;
 //3. sor
 delay(100);
 for i:=17 to 19 do
 	begin
-	gotoxy(i, 4);
+	gotoxy(i, 9);
 	write('*');
 	end;
 for i:=26 to 26 do
 	begin
-	gotoxy(i, 4);
+	gotoxy(i, 9);
 	write('*');
 	end;
 for i:=32 to 36 do
 	begin
-	gotoxy(i, 4);
+	gotoxy(i, 9);
 	write('*');
 	end;
 for i:=42 to 43 do
 	begin
-	gotoxy(i, 4);
+	gotoxy(i, 9);
 	write('*');
 	end;
 for i:=45 to 46 do
 	begin
-	gotoxy(i, 4);
+	gotoxy(i, 9);
 	write('*');
 	end;
 for i:=48 to 49 do
 	begin
-	gotoxy(i, 4);
+	gotoxy(i, 9);
 	write('*');
 	end;
 for i:=51 to 52 do
 	begin
-	gotoxy(i, 4);
+	gotoxy(i, 9);
 	write('*');
 	end;
 for i:=55 to 56 do
 	begin
-	gotoxy(i, 4);
+	gotoxy(i, 9);
 	write('*');
 	end;
 //4. sor
 delay(100);
 for i:=17 to 18 do
 	begin
-	gotoxy(i, 5);
+	gotoxy(i, 10);
 	write('*');
 	end;
 for i:=32 to 33 do
 	begin
-	gotoxy(i, 5);
+	gotoxy(i, 10);
 	write('*');
 	end;
 for i:=35 to 36 do
 	begin
-	gotoxy(i, 5);
+	gotoxy(i, 10);
 	write('*');
 	end;
 for i:=42 to 43 do
 	begin
-	gotoxy(i, 5);
+	gotoxy(i, 10);
 	write('*');
 	end;
 for i:=46 to 48 do
 	begin
-	gotoxy(i, 5);
+	gotoxy(i, 10);
 	write('*');
 	end;
 for i:=51 to 52 do
 	begin
-	gotoxy(i, 5);
+	gotoxy(i, 10);
 	write('*');
 	end;
 for i:=55 to 56 do
 	begin
-	gotoxy(i, 5);
+	gotoxy(i, 10);
 	write('*');
 	end;
 //5.sor
 delay(100);
 for i:=17 to 18 do
 	begin
-	gotoxy(i, 6);
+	gotoxy(i, 11);
 	write('*');
 	end;
 for i:=31 to 32 do
 	begin
-	gotoxy(i, 6);
+	gotoxy(i, 11);
 	write('*');
 	end;
 for i:=36 to 37 do
 	begin
-	gotoxy(i, 6);
+	gotoxy(i, 11);
 	write('*');
 	end;
 for i:=42 to 43 do
 	begin
-	gotoxy(i, 6);
+	gotoxy(i, 11);
 	write('*');
 	end;
 for i:=47 to 47 do
 	begin
-	gotoxy(i, 6);
+	gotoxy(i, 11);
 	write('*');
 	end;
 for i:=51 to 52 do
 	begin
-	gotoxy(i, 6);
+	gotoxy(i, 11);
 	write('*');
 	end;
 for i:=55 to 63 do
 	begin
-	gotoxy(i, 6);
+	gotoxy(i, 11);
 	write('*');
 	end;
 //6. sor
 delay(100);
 for i:=17 to 18 do
 	begin
-	gotoxy(i, 7);
+	gotoxy(i, 12);
 	write('*');
 	end;
 for i:=23 to 26 do
 	begin
-	gotoxy(i, 7);
+	gotoxy(i, 12);
 	write('*');
 	end;
 for i:=31 to 37 do
 	begin
-	gotoxy(i, 7);
+	gotoxy(i, 12);
 	write('*');
 	end;
 for i:=42 to 43 do
 	begin
-	gotoxy(i, 7);
+	gotoxy(i, 12);
 	write('*');
 	end;
 for i:=51 to 52 do
 	begin
-	gotoxy(i, 7);
+	gotoxy(i, 12);
 	write('*');
 	end;
 for i:=55 to 63 do
 	begin
-	gotoxy(i, 7);
+	gotoxy(i, 12);
 	write('*');
 	end;
 //7. sor
 delay(100);
 for i:=17 to 18 do
 	begin
-	gotoxy(i, 8);
+	gotoxy(i, 13);
 	write('*');
 	end;
 for i:=24 to 26 do
 	begin
-	gotoxy(i, 8);
+	gotoxy(i, 13);
 	write('*');
 	end;
 for i:=30 to 31 do
 	begin
-	gotoxy(i, 8);
+	gotoxy(i, 13);
 	write('*');
 	end;
 for i:=32 to 36 do
 	begin
-	gotoxy(i, 8);
+	gotoxy(i, 13);
 	write('*');
 	end;
 for i:=37 to 38 do
 	begin
-	gotoxy(i, 8);
+	gotoxy(i, 13);
 	write('*');
 	end;
 for i:=42 to 43 do
 	begin
-	gotoxy(i, 8);
+	gotoxy(i, 13);
 	write('*');
 	end;
 for i:=51 to 52 do
 	begin
-	gotoxy(i, 8);
+	gotoxy(i, 13);
 	write('*');
 	end;
 for i:=55 to 56 do
 	begin
-	gotoxy(i, 8);
+	gotoxy(i, 13);
 	write('*');
 	end;
 //8. sor
 delay(100);
 for i:=17 to 18 do
 	begin
-	gotoxy(i, 9);
+	gotoxy(i, 14);
 	write('*');
 	end;
 for i:=25 to 26 do
 	begin
-	gotoxy(i, 9);
+	gotoxy(i, 14);
 	write('*');
 	end;
 for i:=30 to 31 do
 	begin
-	gotoxy(i, 9);
+	gotoxy(i, 14);
 	write('*');
 	end;
 for i:=37 to 38 do
 	begin
-	gotoxy(i, 9);
+	gotoxy(i, 14);
 	write('*');
 	end;
 for i:=42 to 43 do
 	begin
-	gotoxy(i, 9);
+	gotoxy(i, 14);
 	write('*');
 	end;
 for i:=51 to 52 do
 	begin
-	gotoxy(i, 9);
+	gotoxy(i, 14);
 	write('*');
 	end;
 for i:=55 to 56 do
 	begin
-	gotoxy(i, 9);
+	gotoxy(i, 14);
 	write('*');
 	end;
 //9. sor
 delay(100);
 for i:=17 to 26 do
 	begin
-	gotoxy(i, 10);
+	gotoxy(i, 15);
 	write('*');
 	end;
 for i:=29 to 30 do
 	begin
-	gotoxy(i, 10);
+	gotoxy(i, 15);
 	write('*');
 	end;
 for i:=38 to 39 do
 	begin
-	gotoxy(i, 10);
+	gotoxy(i, 15);
 	write('*');
 	end;
 for i:=42 to 43 do
 	begin
-	gotoxy(i, 10);
+	gotoxy(i, 15);
 	write('*');
 	end;
 for i:=51 to 52 do
 	begin
-	gotoxy(i, 10);
+	gotoxy(i, 15);
 	write('*');
 	end;
 for i:=55 to 64 do
 	begin
-	gotoxy(i, 10);
+	gotoxy(i, 15);
 	write('*');
 	end;
 //10. sor
 delay(100);
 for i:=17 to 26 do
 	begin
-	gotoxy(i, 11);
+	gotoxy(i, 16);
 	write('*');
 	end;
 for i:=29 to 30 do
 	begin
-	gotoxy(i, 11);
+	gotoxy(i, 16);
 	write('*');
 	end;
 for i:=38 to 39 do
 	begin
-	gotoxy(i, 11);
+	gotoxy(i, 16);
 	write('*');
 	end;
 for i:=42 to 43 do
 	begin
-	gotoxy(i, 11);
+	gotoxy(i, 16);
 	write('*');
 	end;
 for i:=51 to 52 do
 	begin
-	gotoxy(i, 11);
+	gotoxy(i, 16);
 	write('*');
 	end;
 for i:=55 to 64 do
 	begin
-	gotoxy(i, 11);
+	gotoxy(i, 16);
 	write('*');
 	end;
 //szó vége
@@ -373,319 +373,319 @@ for i:=55 to 64 do
 delay(500);
 for i:=18 to 24 do
 	begin
-	gotoxy(i, 15);
+	gotoxy(i, 20);
 	write('*');
 	end;
 for i:=29 to 30 do
 	begin
-	gotoxy(i, 15);
+	gotoxy(i, 20);
 	write('*');
 	end;
 for i:=38 to 39 do
 	begin
-	gotoxy(i, 15);
+	gotoxy(i, 20);
 	write('*');
 	end;
 for i:=42 to 51 do
 	begin
-	gotoxy(i, 15);
+	gotoxy(i, 20);
 	write('*');
 	end;
 for i:=54 to 61 do
 	begin
-	gotoxy(i, 15);
+	gotoxy(i, 20);
 	write('*');
 	end;
 //2. sor
 delay(100);
 for i:=17 to 25 do
 	begin
-	gotoxy(i, 16);
+	gotoxy(i, 21);
 	write('*');
 	end;
 for i:=29 to 30 do
 	begin
-	gotoxy(i, 16);
+	gotoxy(i, 21);
 	write('*');
 	end;
 for i:=38 to 39 do
 	begin
-	gotoxy(i, 16);
+	gotoxy(i, 21);
 	write('*');
 	end;
 for i:=42 to 51 do
 	begin
-	gotoxy(i, 16);
+	gotoxy(i, 21);
 	write('*');
 	end;
 for i:=54 to 62 do
 	begin
-	gotoxy(i, 16);
+	gotoxy(i, 21);
 	write('*');
 	end;
 //3. sor
 delay(100);
 for i:=16 to 18 do
 	begin
-	gotoxy(i, 17);
+	gotoxy(i, 22);
 	write('*');
 	end;
 for i:=24 to 26 do
 	begin
-	gotoxy(i, 17);
+	gotoxy(i, 22);
 	write('*');
 	end;
 for i:=30 to 31 do
 	begin
-	gotoxy(i, 17);
+	gotoxy(i, 22);
 	write('*');
 	end;
 for i:=37 to 38 do
 	begin
-	gotoxy(i, 17);
+	gotoxy(i, 22);
 	write('*');
 	end;
 for i:=42 to 43 do
 	begin
-	gotoxy(i, 17);
+	gotoxy(i, 22);
 	write('*');
 	end;
 for i:=54 to 56 do
 	begin
-	gotoxy(i, 17);
+	gotoxy(i, 22);
 	write('*');
 	end;
 for i:=60 to 63 do
 	begin
-	gotoxy(i, 17);
+	gotoxy(i, 22);
 	write('*');
 	end;
 //4. sor
 delay(100);
 for i:=16 to 17 do
 	begin
-	gotoxy(i, 18);
+	gotoxy(i, 23);
 	write('*');
 	end;
 for i:=25 to 26 do
 	begin
-	gotoxy(i, 18);
+	gotoxy(i, 23);
 	write('*');
 	end;
 for i:=30 to 31 do
 	begin
-	gotoxy(i, 18);
+	gotoxy(i, 23);
 	write('*');
 	end;
 for i:=37 to 38 do
 	begin
-	gotoxy(i, 18);
+	gotoxy(i, 23);
 	write('*');
 	end;
 for i:=42 to 43 do
 	begin
-	gotoxy(i, 18);
+	gotoxy(i, 23);
 	write('*');
 	end;
 for i:=54 to 56 do
 	begin
-	gotoxy(i, 18);
+	gotoxy(i, 23);
 	write('*');
 	end;
 for i:=60 to 62 do
 	begin
-	gotoxy(i, 18);
+	gotoxy(i, 23);
 	write('*');
 	end;
 //5. sor
 delay(100);
 for i:=16 to 17 do
 	begin
-	gotoxy(i, 19);
+	gotoxy(i, 24);
 	write('*');
 	end;
 for i:=25 to 26 do
 	begin
-	gotoxy(i, 19);
+	gotoxy(i, 24);
 	write('*');
 	end;
 for i:=31 to 32 do
 	begin
-	gotoxy(i, 19);
+	gotoxy(i, 24);
 	write('*');
 	end;
 for i:=36 to 37 do
 	begin
-	gotoxy(i, 19);
+	gotoxy(i, 24);
 	write('*');
 	end;
 for i:=42 to 50 do
 	begin
-	gotoxy(i, 19);
+	gotoxy(i, 24);
 	write('*');
 	end;
 for i:=54 to 61 do
 	begin
-	gotoxy(i, 19);
+	gotoxy(i, 24);
 	write('*');
 	end;
 //6. sor
 delay(100);
 for i:=16 to 17 do
 	begin
-	gotoxy(i, 20);
+	gotoxy(i, 25);
 	write('*');
 	end;
 for i:=25 to 26 do
 	begin
-	gotoxy(i, 20);
+	gotoxy(i, 25);
 	write('*');
 	end;
 for i:=31 to 32 do
 	begin
-	gotoxy(i, 20);
+	gotoxy(i, 25);
 	write('*');
 	end;
 for i:=36 to 37 do
 	begin
-	gotoxy(i, 20);
+	gotoxy(i, 25);
 	write('*');
 	end;
 for i:=42 to 50 do
 	begin
-	gotoxy(i, 20);
+	gotoxy(i, 25);
 	write('*');
 	end;
 for i:=54 to 60 do
 	begin
-	gotoxy(i, 20);
+	gotoxy(i, 25);
 	write('*');
 	end;
 //7. sor
 delay(100);
 for i:=16 to 17 do
 	begin
-	gotoxy(i, 21);
+	gotoxy(i, 26);
 	write('*');
 	end;
 for i:=25 to 26 do
 	begin
-	gotoxy(i, 21);
+	gotoxy(i, 26);
 	write('*');
 	end;
 for i:=32 to 33 do
 	begin
-	gotoxy(i, 21);
+	gotoxy(i, 26);
 	write('*');
 	end;
 for i:=35 to 36 do
 	begin
-	gotoxy(i, 21);
+	gotoxy(i, 26);
 	write('*');
 	end;
 for i:=42 to 43 do
 	begin
-	gotoxy(i, 21);
+	gotoxy(i, 26);
 	write('*');
 	end;
 for i:=54 to 56 do
 	begin
-	gotoxy(i, 21);
+	gotoxy(i, 26);
 	write('*');
 	end;
 for i:=58 to 61 do
 	begin
-	gotoxy(i, 21);
+	gotoxy(i, 26);
 	write('*');
 	end;
 //8. sor
 delay(100);
 for i:=16 to 18 do
 	begin
-	gotoxy(i, 22);
+	gotoxy(i, 27);
 	write('*');
 	end;
 for i:=24 to 26 do
 	begin
-	gotoxy(i, 22);
+	gotoxy(i, 27);
 	write('*');
 	end;
 for i:=24 to 26 do
 	begin
-	gotoxy(i, 22);
+	gotoxy(i, 27);
 	write('*');
 	end;
 for i:=32 to 36 do
 	begin
-	gotoxy(i, 22);
+	gotoxy(i, 27);
 	write('*');
 	end;
 for i:=42 to 43 do
 	begin
-	gotoxy(i, 22);
+	gotoxy(i, 27);
 	write('*');
 	end;
 for i:=54 to 56 do
 	begin
-	gotoxy(i, 22);
+	gotoxy(i, 27);
 	write('*');
 	end;
 for i:=60 to 62 do
 	begin
-	gotoxy(i, 22);
+	gotoxy(i, 27);
 	write('*');
 	end;
 //9. sor
 delay(100);
 for i:=17 to 25 do
 	begin
-	gotoxy(i, 23);
+	gotoxy(i, 28);
 	write('*');
 	end;
 for i:=33 to 35 do
 	begin
-	gotoxy(i, 23);
+	gotoxy(i, 28);
 	write('*');
 	end;
 for i:=42 to 51 do
 	begin
-	gotoxy(i, 23);
+	gotoxy(i, 28);
 	write('*');
 	end;
 for i:=54 to 56 do
 	begin
-	gotoxy(i, 23);
+	gotoxy(i, 28);
 	write('*');
 	end;
 for i:=61 to 63 do
 	begin
-	gotoxy(i, 23);
+	gotoxy(i, 28);
 	write('*');
 	end;
 //10. sor
 for i:=18 to 24 do
 	begin
-	gotoxy(i, 24);
+	gotoxy(i, 29);
 	write('*');
 	end;
 for i:=34 to 34 do
 	begin
-	gotoxy(i, 24);
+	gotoxy(i, 29);
 	write('*');
 	end;
 for i:=42 to 51 do
 	begin
-	gotoxy(i, 24);
+	gotoxy(i, 29);
 	write('*');
 	end;
 for i:=54 to 56 do
 	begin
-	gotoxy(i, 24);
+	gotoxy(i, 29);
 	write('*');
 	end;
 for i:=62 to 64 do
 	begin
-	gotoxy(i, 24);
+	gotoxy(i, 29);
 	write('*');
 	end;
 //vége
@@ -1432,6 +1432,7 @@ end;		//beállítások-----------------------------------------------
 
 if (fo=1) then
 begin		//alap------------------------------------------------------
+lepes:=1;
 palya(mag, szel);
 x:=(szel div 2);
 y:=(mag div 2);
@@ -1550,8 +1551,11 @@ if (x<2) or (x>szel-1) or (y<2) or(y>mag-1)
 				 gotoxy(x, y);
 				 end
 			else begin 
-				 gotoxy(memx[maxx], memy[maxy]);
-				 write(' ');
+				 if (lepes+1>=maxx)
+				 then begin
+					  gotoxy(memx[maxx], memy[maxy]);
+					  write(' ');
+					  end;
 				 end;
 		 end;
 
@@ -1560,6 +1564,7 @@ if not (x<2) and not (y<2) and not (x>szel-1) and not (y>mag-1)
 	then begin
 		 gotoxy(x, y);
 		 write('*');
+		 lepes:=lepes+1;
 		 
 		 if (gyor=1) then delay(500);
 		 if (gyor=2) then delay(200);
