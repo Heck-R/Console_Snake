d8f1f8f42d9406dcb4e4582923cccddbefcdde2c
Heck-R <hecvid97@gmail.com>
2019-08-07 20:16:07 +0200

    Update: Optimized movement Added: Game Over logo for future

--- Snake/Snake.pas
+++ Snake/Snake.pas
@@ -10,11 +10,689 @@ var i, j, k:integer;
 var xt: array[2..max] of integer;
 var yt: array[2..max] of integer;
 
-begin	
+
+procedure over;
+var i:integer;
+begin
+//1. sor
+delay(100);
+for i:=17 to 26 do
+	begin
+	gotoxy(i, 2);
+	write('*');
+	end;
+for i:=34 to 34 do
+	begin
+	gotoxy(i, 2);
+	write('*');
+	end;
+for i:=42 to 45 do
+	begin
+	gotoxy(i, 2);
+	write('*');
+	end;
+for i:=49 to 52 do
+	begin
+	gotoxy(i, 2);
+	write('*');
+	end;
+for i:=55 to 64 do
+	begin
+	gotoxy(i, 2);
+	write('*');
+	end;
+//2. sor
+delay(100);
+for i:=17 to 26 do
+	begin
+	gotoxy(i, 3);
+	write('*');
+	end;
+for i:=33 to 35 do
+	begin
+	gotoxy(i, 3);
+	write('*');
+	end;
+for i:=42 to 46 do
+	begin
+	gotoxy(i, 3);
+	write('*');
+	end;
+for i:=48 to 52 do
+	begin
+	gotoxy(i, 3);
+	write('*');
+	end;
+for i:=55 to 64 do
+	begin
+	gotoxy(i, 3);
+	write('*');
+	end;
+//3. sor
+delay(100);
+for i:=17 to 19 do
+	begin
+	gotoxy(i, 4);
+	write('*');
+	end;
+for i:=26 to 26 do
+	begin
+	gotoxy(i, 4);
+	write('*');
+	end;
+for i:=32 to 36 do
+	begin
+	gotoxy(i, 4);
+	write('*');
+	end;
+for i:=42 to 43 do
+	begin
+	gotoxy(i, 4);
+	write('*');
+	end;
+for i:=45 to 46 do
+	begin
+	gotoxy(i, 4);
+	write('*');
+	end;
+for i:=48 to 49 do
+	begin
+	gotoxy(i, 4);
+	write('*');
+	end;
+for i:=51 to 52 do
+	begin
+	gotoxy(i, 4);
+	write('*');
+	end;
+for i:=55 to 56 do
+	begin
+	gotoxy(i, 4);
+	write('*');
+	end;
+//4. sor
+delay(100);
+for i:=17 to 18 do
+	begin
+	gotoxy(i, 5);
+	write('*');
+	end;
+for i:=32 to 33 do
+	begin
+	gotoxy(i, 5);
+	write('*');
+	end;
+for i:=35 to 36 do
+	begin
+	gotoxy(i, 5);
+	write('*');
+	end;
+for i:=42 to 43 do
+	begin
+	gotoxy(i, 5);
+	write('*');
+	end;
+for i:=46 to 48 do
+	begin
+	gotoxy(i, 5);
+	write('*');
+	end;
+for i:=51 to 52 do
+	begin
+	gotoxy(i, 5);
+	write('*');
+	end;
+for i:=55 to 56 do
+	begin
+	gotoxy(i, 5);
+	write('*');
+	end;
+//5.sor
+delay(100);
+for i:=17 to 18 do
+	begin
+	gotoxy(i, 6);
+	write('*');
+	end;
+for i:=31 to 32 do
+	begin
+	gotoxy(i, 6);
+	write('*');
+	end;
+for i:=36 to 37 do
+	begin
+	gotoxy(i, 6);
+	write('*');
+	end;
+for i:=42 to 43 do
+	begin
+	gotoxy(i, 6);
+	write('*');
+	end;
+for i:=47 to 47 do
+	begin
+	gotoxy(i, 6);
+	write('*');
+	end;
+for i:=51 to 52 do
+	begin
+	gotoxy(i, 6);
+	write('*');
+	end;
+for i:=55 to 63 do
+	begin
+	gotoxy(i, 6);
+	write('*');
+	end;
+//6. sor
+delay(100);
+for i:=17 to 18 do
+	begin
+	gotoxy(i, 7);
+	write('*');
+	end;
+for i:=23 to 26 do
+	begin
+	gotoxy(i, 7);
+	write('*');
+	end;
+for i:=31 to 37 do
+	begin
+	gotoxy(i, 7);
+	write('*');
+	end;
+for i:=42 to 43 do
+	begin
+	gotoxy(i, 7);
+	write('*');
+	end;
+for i:=51 to 52 do
+	begin
+	gotoxy(i, 7);
+	write('*');
+	end;
+for i:=55 to 63 do
+	begin
+	gotoxy(i, 7);
+	write('*');
+	end;
+//7. sor
+delay(100);
+for i:=17 to 18 do
+	begin
+	gotoxy(i, 8);
+	write('*');
+	end;
+for i:=24 to 26 do
+	begin
+	gotoxy(i, 8);
+	write('*');
+	end;
+for i:=30 to 31 do
+	begin
+	gotoxy(i, 8);
+	write('*');
+	end;
+for i:=32 to 36 do
+	begin
+	gotoxy(i, 8);
+	write('*');
+	end;
+for i:=37 to 38 do
+	begin
+	gotoxy(i, 8);
+	write('*');
+	end;
+for i:=42 to 43 do
+	begin
+	gotoxy(i, 8);
+	write('*');
+	end;
+for i:=51 to 52 do
+	begin
+	gotoxy(i, 8);
+	write('*');
+	end;
+for i:=55 to 56 do
+	begin
+	gotoxy(i, 8);
+	write('*');
+	end;
+//8. sor
+delay(100);
+for i:=17 to 18 do
+	begin
+	gotoxy(i, 9);
+	write('*');
+	end;
+for i:=25 to 26 do
+	begin
+	gotoxy(i, 9);
+	write('*');
+	end;
+for i:=30 to 31 do
+	begin
+	gotoxy(i, 9);
+	write('*');
+	end;
+for i:=37 to 38 do
+	begin
+	gotoxy(i, 9);
+	write('*');
+	end;
+for i:=42 to 43 do
+	begin
+	gotoxy(i, 9);
+	write('*');
+	end;
+for i:=51 to 52 do
+	begin
+	gotoxy(i, 9);
+	write('*');
+	end;
+for i:=55 to 56 do
+	begin
+	gotoxy(i, 9);
+	write('*');
+	end;
+//9. sor
+delay(100);
+for i:=17 to 26 do
+	begin
+	gotoxy(i, 10);
+	write('*');
+	end;
+for i:=29 to 30 do
+	begin
+	gotoxy(i, 10);
+	write('*');
+	end;
+for i:=38 to 39 do
+	begin
+	gotoxy(i, 10);
+	write('*');
+	end;
+for i:=42 to 43 do
+	begin
+	gotoxy(i, 10);
+	write('*');
+	end;
+for i:=51 to 52 do
+	begin
+	gotoxy(i, 10);
+	write('*');
+	end;
+for i:=55 to 64 do
+	begin
+	gotoxy(i, 10);
+	write('*');
+	end;
+//10. sor
+delay(100);
+for i:=17 to 26 do
+	begin
+	gotoxy(i, 11);
+	write('*');
+	end;
+for i:=29 to 30 do
+	begin
+	gotoxy(i, 11);
+	write('*');
+	end;
+for i:=38 to 39 do
+	begin
+	gotoxy(i, 11);
+	write('*');
+	end;
+for i:=42 to 43 do
+	begin
+	gotoxy(i, 11);
+	write('*');
+	end;
+for i:=51 to 52 do
+	begin
+	gotoxy(i, 11);
+	write('*');
+	end;
+for i:=55 to 64 do
+	begin
+	gotoxy(i, 11);
+	write('*');
+	end;
+//szó vége
+
+//2. szó
+//1. sor
+delay(500);
+for i:=18 to 24 do
+	begin
+	gotoxy(i, 15);
+	write('*');
+	end;
+for i:=29 to 30 do
+	begin
+	gotoxy(i, 15);
+	write('*');
+	end;
+for i:=38 to 39 do
+	begin
+	gotoxy(i, 15);
+	write('*');
+	end;
+for i:=42 to 51 do
+	begin
+	gotoxy(i, 15);
+	write('*');
+	end;
+for i:=54 to 61 do
+	begin
+	gotoxy(i, 15);
+	write('*');
+	end;
+//2. sor
+delay(100);
+for i:=17 to 25 do
+	begin
+	gotoxy(i, 16);
+	write('*');
+	end;
+for i:=29 to 30 do
+	begin
+	gotoxy(i, 16);
+	write('*');
+	end;
+for i:=38 to 39 do
+	begin
+	gotoxy(i, 16);
+	write('*');
+	end;
+for i:=42 to 51 do
+	begin
+	gotoxy(i, 16);
+	write('*');
+	end;
+for i:=54 to 62 do
+	begin
+	gotoxy(i, 16);
+	write('*');
+	end;
+//3. sor
+delay(100);
+for i:=16 to 18 do
+	begin
+	gotoxy(i, 17);
+	write('*');
+	end;
+for i:=24 to 26 do
+	begin
+	gotoxy(i, 17);
+	write('*');
+	end;
+for i:=30 to 31 do
+	begin
+	gotoxy(i, 17);
+	write('*');
+	end;
+for i:=37 to 38 do
+	begin
+	gotoxy(i, 17);
+	write('*');
+	end;
+for i:=42 to 43 do
+	begin
+	gotoxy(i, 17);
+	write('*');
+	end;
+for i:=54 to 56 do
+	begin
+	gotoxy(i, 17);
+	write('*');
+	end;
+for i:=60 to 63 do
+	begin
+	gotoxy(i, 17);
+	write('*');
+	end;
+//4. sor
+delay(100);
+for i:=16 to 17 do
+	begin
+	gotoxy(i, 18);
+	write('*');
+	end;
+for i:=25 to 26 do
+	begin
+	gotoxy(i, 18);
+	write('*');
+	end;
+for i:=30 to 31 do
+	begin
+	gotoxy(i, 18);
+	write('*');
+	end;
+for i:=37 to 38 do
+	begin
+	gotoxy(i, 18);
+	write('*');
+	end;
+for i:=42 to 43 do
+	begin
+	gotoxy(i, 18);
+	write('*');
+	end;
+for i:=54 to 56 do
+	begin
+	gotoxy(i, 18);
+	write('*');
+	end;
+for i:=60 to 62 do
+	begin
+	gotoxy(i, 18);
+	write('*');
+	end;
+//5. sor
+delay(100);
+for i:=16 to 17 do
+	begin
+	gotoxy(i, 19);
+	write('*');
+	end;
+for i:=25 to 26 do
+	begin
+	gotoxy(i, 19);
+	write('*');
+	end;
+for i:=31 to 32 do
+	begin
+	gotoxy(i, 19);
+	write('*');
+	end;
+for i:=36 to 37 do
+	begin
+	gotoxy(i, 19);
+	write('*');
+	end;
+for i:=42 to 50 do
+	begin
+	gotoxy(i, 19);
+	write('*');
+	end;
+for i:=54 to 61 do
+	begin
+	gotoxy(i, 19);
+	write('*');
+	end;
+//6. sor
+delay(100);
+for i:=16 to 17 do
+	begin
+	gotoxy(i, 20);
+	write('*');
+	end;
+for i:=25 to 26 do
+	begin
+	gotoxy(i, 20);
+	write('*');
+	end;
+for i:=31 to 32 do
+	begin
+	gotoxy(i, 20);
+	write('*');
+	end;
+for i:=36 to 37 do
+	begin
+	gotoxy(i, 20);
+	write('*');
+	end;
+for i:=42 to 50 do
+	begin
+	gotoxy(i, 20);
+	write('*');
+	end;
+for i:=54 to 60 do
+	begin
+	gotoxy(i, 20);
+	write('*');
+	end;
+//7. sor
+delay(100);
+for i:=16 to 17 do
+	begin
+	gotoxy(i, 21);
+	write('*');
+	end;
+for i:=25 to 26 do
+	begin
+	gotoxy(i, 21);
+	write('*');
+	end;
+for i:=32 to 33 do
+	begin
+	gotoxy(i, 21);
+	write('*');
+	end;
+for i:=35 to 36 do
+	begin
+	gotoxy(i, 21);
+	write('*');
+	end;
+for i:=42 to 43 do
+	begin
+	gotoxy(i, 21);
+	write('*');
+	end;
+for i:=54 to 56 do
+	begin
+	gotoxy(i, 21);
+	write('*');
+	end;
+for i:=58 to 61 do
+	begin
+	gotoxy(i, 21);
+	write('*');
+	end;
+//8. sor
+delay(100);
+for i:=16 to 18 do
+	begin
+	gotoxy(i, 22);
+	write('*');
+	end;
+for i:=24 to 26 do
+	begin
+	gotoxy(i, 22);
+	write('*');
+	end;
+for i:=24 to 26 do
+	begin
+	gotoxy(i, 22);
+	write('*');
+	end;
+for i:=32 to 36 do
+	begin
+	gotoxy(i, 22);
+	write('*');
+	end;
+for i:=42 to 43 do
+	begin
+	gotoxy(i, 22);
+	write('*');
+	end;
+for i:=54 to 56 do
+	begin
+	gotoxy(i, 22);
+	write('*');
+	end;
+for i:=60 to 62 do
+	begin
+	gotoxy(i, 22);
+	write('*');
+	end;
+//9. sor
+delay(100);
+for i:=17 to 25 do
+	begin
+	gotoxy(i, 23);
+	write('*');
+	end;
+for i:=33 to 35 do
+	begin
+	gotoxy(i, 23);
+	write('*');
+	end;
+for i:=42 to 51 do
+	begin
+	gotoxy(i, 23);
+	write('*');
+	end;
+for i:=54 to 56 do
+	begin
+	gotoxy(i, 23);
+	write('*');
+	end;
+for i:=61 to 63 do
+	begin
+	gotoxy(i, 23);
+	write('*');
+	end;
+//10. sor
+for i:=18 to 24 do
+	begin
+	gotoxy(i, 24);
+	write('*');
+	end;
+for i:=34 to 34 do
+	begin
+	gotoxy(i, 24);
+	write('*');
+	end;
+for i:=42 to 51 do
+	begin
+	gotoxy(i, 24);
+	write('*');
+	end;
+for i:=54 to 56 do
+	begin
+	gotoxy(i, 24);
+	write('*');
+	end;
+for i:=62 to 64 do
+	begin
+	gotoxy(i, 24);
+	write('*');
+	end;
+//vége
+writeln;
+end;
+
+
+
+BEGIN	
 x:=10;
 y:=10;
 gotoxy(x, y);
 write('*');
+j:=max+1;
 for i:=2 to max do
 	begin
 	xt[i]:=10;
@@ -22,63 +700,59 @@ for i:=2 to max do
 	end;
 c:=readkey;
 
-	repeat
-		
-		
-		clrscr;
-		
-		if keypressed 
-			then begin
-				 c:=readkey 
-				 end;
-		
-				 case ord(c) of
-				 72: y:=y-1;	//fel
-				 80: y:=y+1;	//le
-				 77: x:=x+1;	//jobb
-				 75: x:=x-1;	//bal
-				 end;
-		
-		
-		
-		if (x<1) or (x>80) or (y<1) or(y>25)
-		then begin
-			 
-			 end
-		else begin;
-			 
-			 for j:=max downto 3 do
-			 begin
-			 xt[j]:=xt[j-1];
-			 yt[j]:=yt[j-1];
-			 end;
-			 
-			 xt[2]:=x;
-			 
-			 yt[2]:=y;
-			 end;
-		
-		if x < 1 then x:=1;
-		if x > 80 then x:=80;
-		if y < 1 then y:=1;
-		if y > 25 then y:=25;
-		
-		for k:=max downto 2 do
-		begin
-		gotoxy(xt[k], yt[k]);
-		write('*');
-		end;
 
-		gotoxy(x, y);
-		write('*');
-		
-		delay(100);
-		
-		if keypressed 
-			then begin
-				 c:=readkey 
-				 end;
-		
+repeat
+	
+	
+	
+	
+	if keypressed 
+		then begin
+			 c:=readkey 
+			 end;
+	
+			 case ord(c) of
+			 72: y:=y-1;	//fel
+			 80: y:=y+1;	//le
+			 77: x:=x+1;	//jobb
+			 75: x:=x-1;	//bal
+			 end;
+	
+	
+	
+	if (x<1) or (x>80) or (y<1) or(y>25)
+	then begin
+		 
+		 end
+	else begin;
+		 
+		 j:=j-1;
+		 if j=1 then j:=max;
+		 
+		 gotoxy(xt[j], yt[j]);
+		 write(' ');
+		 
+		 xt[j]:=x;
+		 yt[j]:=y;
+		 
+		 end;
+	
+	if x < 1 then x:=1;
+	if x > 80 then x:=80;
+	if y < 1 then y:=1;
+	if y > 25 then y:=25;
+	
+	
+	gotoxy(x, y);
+	write('*');
+	
+	delay(100);
+	
+	if keypressed 
+		then begin
+			 c:=readkey 
+			 end;
+	
 	until (c in['v', 'V']); 
 	
-end.
+END.
